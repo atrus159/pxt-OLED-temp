c2c80c6c4eaef9d216ad2690e9e2f3795f31f893
atrus159 <darius.is.ru@gmail.com>
2019-07-09 15:41:08 +0800

    Updates.

--- main.ts
+++ main.ts
@@ -1,4 +1,30 @@
 namespace OLED {
+    
+    const SSD1306_SETCONTRAST = 0x81
+    const SSD1306_DISPLAYALLON_RESUME = 0xA4
+    const SSD1306_DISPLAYALLON = 0xA5
+    const SSD1306_NORMALDISPLAY = 0xA6
+    const SSD1306_INVERTDISPLAY = 0xA7
+    const SSD1306_DISPLAYOFF = 0xAE
+    const SSD1306_DISPLAYON = 0xAF
+    const SSD1306_SETDISPLAYOFFSET = 0xD3
+    const SSD1306_SETCOMPINS = 0xDA
+    const SSD1306_SETVCOMDETECT = 0xDB
+    const SSD1306_SETDISPLAYCLOCKDIV = 0xD5
+    const SSD1306_SETPRECHARGE = 0xD9
+    const SSD1306_SETMULTIPLEX = 0xA8
+    const SSD1306_SETLOWCOLUMN = 0x00
+    const SSD1306_SETHIGHCOLUMN = 0x10
+    const SSD1306_SETSTARTLINE = 0x40
+    const SSD1306_MEMORYMODE = 0x20
+    const SSD1306_COMSCANINC = 0xC0
+    const SSD1306_COMSCANDEC = 0xC8
+    const SSD1306_SEGREMAP = 0xA0
+    const SSD1306_CHARGEPUMP = 0x8D
+
+    function command(cmd: number){
+        
+    }
 
     export function init(width: number, height: number) {
         let buf = pins.createBuffer(2)
