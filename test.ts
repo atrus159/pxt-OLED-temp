d6dd8dffc09acbbb4351289a263edc9a4d528d47
atrus159 <darius.is.ru@gmail.com>
2019-07-11 12:08:22 +0800

    Updates.

--- test.ts
+++ test.ts
@@ -1,3 +1,10 @@
 // tests go here; this will not be compiled when this package is used as a library
 OLED.init(64, 128)
 OLED.fill()
+basic.showLeds(`
+        # . . . #
+        . # # # .
+        . # # # .
+        . # # # .
+        # . . . #
+        `)
