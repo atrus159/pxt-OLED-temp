69c4b2edf57ead2f560cfd54071bf04466094850
atrus159 <darius.is.ru@gmail.com>
2019-07-16 12:18:17 +0800

    Updates.

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
 // tests go here; this will not be compiled when this package is used as a library
 
-OLED.init()
-//OLED.clear(true)
-//OLED.drawChar(100, 2, "x")
+OLED.init(128, 64)
+OLED.clear(false)
+OLED.drawChar(100, 2, "x")
